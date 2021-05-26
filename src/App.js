21e22741c63d31a095abdec032ebac1e5e566097
ForgeSolutions-JBrenner <justin.brenner@forgetechnologysolutions.com>
2021-05-26 16:00:46 -0400

    aws authentication added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react'
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import {Button, Navbar, Form, FormControl, Nav} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <AmplifySignOut width="20px"/>
+        </Form>
+      </Navbar>
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-          <h1>Version 2 has been deployed</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
